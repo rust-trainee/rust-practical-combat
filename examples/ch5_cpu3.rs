8adf8e7df70b71c15214e3a582e2f451b91d15c1
hubvue <cckcc.kim@gmail.com>
2024-12-10 11:51:11 +0800

    feat: cpu_3

---
+++ examples/ch5_cpu3.rs
@@ -0,0 +1,121 @@
+/// # 调用函数
+/// ## 给 CPU 扩展出对于栈的支持能力
+/// 一些操作码的定义
+/// - 函数调用:使用 CALL 操作码(0x2 nnn, 其中 nnn 是一个内存地址), 把 position_in_memory设置为nnn 是函数地址
+/// - 函数返回: 使用 RETURN 操作码(0x00EE), 把 position_in_mempory 设置成为函数调用之前的地址.
+
+struct CPU {
+    registers: [u8; 16],
+    position_in_memory: usize,
+    memory: [u8; 4096],
+    // 栈的最大高度是16, 在16 次嵌套函数调用后,次程序将会发生栈溢出.
+    stack: [u16; 16],
+    // 把 stack_pointer 的类型设置为 usize, 可以让 stack 中索引值的操作变得更容易
+    stack_pointer: usize,
+}
+
+impl CPU {
+    fn read_opcode(&self) -> u16 {
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        op_byte1 << 8 | op_byte2
+    }
+    fn run(&mut self) {
+        loop {
+            let opcode = self.read_opcode();
+            self.position_in_memory += 2;
+
+            let c = ((opcode & 0xF000) >> 12) as u8;
+            let x = ((opcode & 0x0F00) >> 8) as u8;
+            let y = ((opcode & 0x00F0) >> 4) as u8;
+            let d = ((opcode & 0x000F) >> 0) as u8;
+
+            let nnn = opcode & 0x0FFF;
+
+            match (c, x, y, d) {
+                (0, 0, 0, 0) => {
+                    return;
+                }
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
+                _ => todo!("opcode {:04x}", opcode),
+            }
+        }
+    }
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow!")
+        }
+
+        stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow")
+        }
+
+        self.stack_pointer -= 1;
+        let addr = self.stack[self.stack_pointer];
+        self.position_in_memory = addr as usize;
+    }
+    fn add_xy(&mut self, x: u8, y: u8) {
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow_detected) = arg1.overflowing_add(arg2);
+
+        self.registers[x as usize] = val;
+
+        if overflow_detected {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
+    }
+}
+
+fn main() {
+    let mut cpu = CPU {
+        registers: [0; 16],
+        memory: [0; 4096],
+        position_in_memory: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
+    };
+
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+
+    let mem = &mut cpu.memory;
+    mem[0x000] = 0x21;
+    mem[0x001] = 0x00;
+
+    mem[0x002] = 0x21;
+    mem[0x003] = 0x00;
+
+    mem[0x004] = 0x00;
+    mem[0x005] = 0x00;
+
+    mem[0x100] = 0x80;
+    mem[0x101] = 0x14;
+
+    mem[0x102] = 0x80;
+    mem[0x103] = 0x14;
+
+    mem[0x104] = 0x00;
+    mem[0x105] = 0xEE;
+
+    cpu.run();
+
+    assert_eq!(cpu.registers[0], 45);
+
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
+}
