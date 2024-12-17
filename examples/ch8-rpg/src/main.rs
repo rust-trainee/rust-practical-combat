28e5b9598d39393e9433338763d9b16ea6a497ed
hubvue <cckcc.kim@gmail.com>
2024-12-17 23:44:40 +0800

    feat: ch9-rpg

--- examples/ch8-rpg/src/main.rs
+++ examples/ch8-rpg/src/main.rs
@@ -1,3 +1,66 @@
+use rand::{self, seq::SliceRandom, Rng};
+trait Enchanter: std::fmt::Debug {
+    fn competency(&self) -> f64;
+
+    fn enchant(&self, thing: &mut Thing) {
+        let probability_of_success = self.competency();
+        let spell_is_successful = rand::thread_rng().gen_bool(probability_of_success);
+
+        print!("{:?} mutters incoherently.", self);
+
+        if spell_is_successful {
+            println!("The {:?} glows brightly", thing);
+        } else {
+            println!(
+                "The {:?} fizzes, then turns into a worthless trinket.",
+                thing
+            );
+            *thing = Thing::Trinket {};
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Dwarf {}
+
+impl Enchanter for Dwarf {
+    fn competency(&self) -> f64 {
+        0.8
+    }
+}
+
+#[derive(Debug)]
+struct Elf {}
+
+impl Enchanter for Elf {
+    fn competency(&self) -> f64 {
+        0.95
+    }
+}
+
+#[derive(Debug)]
+struct Human {}
+
+impl Enchanter for Human {
+    fn competency(&self) -> f64 {
+        0.8
+    }
+}
+
+#[derive(Debug)]
+enum Thing {
+    Sword,
+    Trinket,
+}
 fn main() {
-    println!("Hello, world!");
+    let mut it = Thing::Sword;
+
+    let d = Dwarf {};
+    let e = Elf {};
+    let h = Human {};
+
+    let party: Vec<&dyn Enchanter> = vec![&d, &h, &e];
+
+    let spellcaster = party.choose(&mut rand::thread_rng()).unwrap();
+    spellcaster.enchant(&mut it);
 }
